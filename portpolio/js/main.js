fca46089f72b8681f37dda644e0c4f8da13f8515
junsung-park97 <not971006@gmail.com>
2025-02-06 18:03:59 +0900

    250206 portpolio

--- portpolio/js/main.js
+++ portpolio/js/main.js
@@ -1,4 +1,49 @@
 $(document).ready(function(){
     
+    const swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: true,
+        },
+
+        //effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
+        
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.swiper-button-next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.swiper-button-prev',  
+        },
+
+    });
+    swiper.autoplay.stop();  /* 일시정지 기능 */
+    swiper.autoplay.start();  /* 재생 기능 */
+
+
+    $('.exhibit .ex_tab_cnt .ex_tabs ul li button').on('click',function(){
+        $('.exhibit .ex_tab_cnt .ex_tabs ul li button').removeClass('on')
+        $(this).addClass('on')
+    })
+
+    $('.collrection .coll_cnt .right ul li a').on('mouseenter',function(){
+        // console.log('yes')
+        $('.collrection .coll_cnt .right ul li a').removeClass('over')
+        $(this).addClass('over')
+    })
+    $('.collrection .coll_cnt .right ul li').on('mouseleave',function(){
+        //console.log('yes')
+        $('.collrection .coll_cnt .right ul li a').removeClass('over')
+    })
 
 })
