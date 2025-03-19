2482b9a5cc43dcf8322f6d0ac136f27a8e607536
junsung-park97 <not971006@gmail.com>
2025-03-19 13:06:07 +0900

    산역상담 및 견적서
    마크업 및 스타일시트 작성 우체국api 및 도착예정시간을 선택할 때 시간을 드롭박스로 구현할 지 아님 유저가 선택하도록 할 것인지 생각해보고 구현

---
+++ pro01/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+
+
+    $('.tombstone_estimate .tombstone_list .tombstone_infor .btn_list button').on('click', function () {
+        $(this).toggleClass('active');
+    });
+    
+})
