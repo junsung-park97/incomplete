5cdd99328955924528a591f4a1fa8dac536c0703
junsung-park97 <not971006@gmail.com>
2025-03-20 10:56:34 +0900

    위치정보제공 api
    카카오 주소검색 api를 호출하는 과정에서 url을 알수없다는 오류가 발생함 그래서 일단 api호출관련된 코드를 전부 지웠는데 기존에 존재하던 인터렉티브 요소중 버튼에 클래스를 추가하였던게 선택자 오류가 뜨기 시작함

--- pro01/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-
-    $('.tombstone_estimate .tombstone_list .tombstone_infor .btn_list button').on('click', function () {
-        $(this).toggleClass('active');
-    });
-    
-
-})
